0e3491d0ed979fed7b58debc8c33831f074e9259
Kesiyapriya M <kesiyapriya1999@gmail.com>
2024-10-06 15:50:51 +0530

    Product Display

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React ,{ useState }from 'react'
 import { assets } from '../assets/assets'
 import { Link, NavLink } from 'react-router-dom'
-import { useState } from 'react'
+
 
 const Navbar = () => {
 
@@ -9,7 +9,8 @@ const Navbar = () => {
 
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
-      <img src={assets.logo} className="w-36" alt="" />
+       
+       <Link to='/'><img src={assets.logo} className="w-36" alt="" /></Link>
 
       <ul className='hidden sm:flex gap-5 text-sm text-gray-700 hidden'>
 
@@ -62,6 +63,10 @@ const Navbar = () => {
                   <img className='h-4 rotate-180' src={assets.dropdown_icon} alt="" />
                   <p>Back</p>
               </div>
+              <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/'>HOME</NavLink>
+              <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/collection'>COLLECTION</NavLink>
+              <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/about'>ABOUT</NavLink>
+              <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/contact'>CONTACT</NavLink>
           </div>
 
 
