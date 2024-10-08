9fa42a59bb07b59bb349b57122846c03233e6991
Kesiyapriya M <kesiyapriya1999@gmail.com>
2024-10-08 23:14:44 +0530

    Adding category

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Routes,Route} from 'react-router-dom'
 import Home from './pages/Home'
-import Collection from './pages/Collection'
+import Collection from './pages/collection'
 import About from './pages/About'
 import Contact from './pages/Contact'
 import Product from './pages/Product'
@@ -10,6 +10,7 @@ import Login from './pages/Login'
 import PlaceOrder from './pages/PlaceOrder'
 import Orders from './pages/Orders'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 
 const App = () => {
@@ -26,14 +27,9 @@ const App = () => {
       <Route path='/login' element={<Login/>} />
       <Route path='/place-order' element={<PlaceOrder/>} />
       <Route path='/orders' element={<Orders/>} />
-    </Routes>
-  
-</div>
-  
-  )
-}
-
-export default App
-
-
-
+      </Routes>
+      <Footer/>
+      </div>
+      )
+      }
+      export default App
--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -23,7 +23,7 @@ const LatestCollection = () => {
         </div>
          
          {/* Rendring Products */}
-         <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg;grid-cols-5 gap-y-6'>
+         <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
           {
             latestProducts.map((item,index)=>(
               <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price}/>
@@ -34,4 +34,6 @@ const LatestCollection = () => {
   )
 }
 
+
 export default LatestCollection
+
